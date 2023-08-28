9be2e3119d1a94851ef8ad85e8347f3c24fe0c9c
arulmozhi <arulmozhiasmitha19@gmail.com>
2023-08-28 21:48:33 +0530

    removed

--- SearchMongo/demo/src/main/java/com/skillssearch/demo/repository/PostRepository.java
+++
@@ -1,9 +0,0 @@
-package com.skillssearch.demo.repository;
-
-import com.skillssearch.demo.entity.Post;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-
-public interface PostRepository extends MongoRepository<Post,String> {
-
-}
