c6bc7ff6187994b31b6971bb3e8be5e989110041
arulmozhi <arulmozhiasmitha19@gmail.com>
2023-08-28 21:52:23 +0530

    file added

---
+++ demo/src/main/java/com/skillssearch/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.skillssearch.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
